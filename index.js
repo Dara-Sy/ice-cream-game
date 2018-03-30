330f627311feaf917dbd166220e4308963ee9ee5
darasy <darajanne@gmail.com>
2018-03-29 22:57:19 -0400

    Adding commit changes

---
+++ index.js
@@ -0,0 +1,193 @@
+$(document).ready(function() {
+
+// actual working code
+
+
+// MAKING THE ICE CREAM SCOOPS FALL DOWN
+
+
+function myMove() {
+  var elem = document.getElementById("scoop");
+  var pos = 0;
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (pos == 510) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.marginTop = pos + 'px';
+      // elem.style.left = pos + 'px';
+    }
+    // function sayHi() {
+    //   // setTimeout(function(){ alert("Hello"); }, 3000);
+    //   }
+    //   setTimeout(frame, 1000);
+  }
+}
+
+
+$('button').click(function(e) {
+        $('#landingView').hide();
+        $('#myAnimation').show();  // got a problem with IC showing up
+        $('#gameView').show();
+
+        e.preventDefault();
+        myMove();
+
+    });
+
+
+});
+
+
+
+// MAKING THE CONE MOVE USING ARROW PRESSED FUNCTION
+
+var imageOffset = 0;
+
+function leftArrowPressed() {
+  imageOffset -= 20;
+  // margin-left -=2;
+  var el = document.getElementById("cone");
+  el.style.left = imageOffset + "px";
+
+}
+
+function rightArrowPressed() {
+  imageOffset += 20;
+  var el = document.getElementById("cone");
+  el.style.left = imageOffset + "px";
+}
+
+
+
+function moveSelection(evt) {
+  switch (evt.keyCode) {
+     case 37:
+      leftArrowPressed();
+      console.log('left pressed');
+      break;
+     case 39:
+      rightArrowPressed();
+      console.log('right pressed');
+      break;
+     // case 38:
+     //   upArrowPressed();
+     //   break;
+     // case 40:
+     //   downArrowPressed();
+     //   break;
+    default:
+      break;
+  }
+}
+
+function docReady() {
+  window.addEventListener("keydown", moveSelection);
+}
+
+leftArrowPressed();
+rightArrowPressed();
+docReady();
+
+
+
+// another method to find x, y coordinates and width and height
+
+function move() {
+  // let x = $('#cone').css('left');
+  let y= $('#cone').css('left');
+  // var height = $('#cone').height();
+  // let width = $('#cone').width();
+  console.log(y);
+  // console.log(x);
+  // console.log(height);
+  // console.log(width);
+
+}
+
+// setInterval(function(){
+//   move();
+// }, 1000/60);
+
+
+function name() {
+
+}
+
+
+// Objects - Collision Detection
+// find coordinates of scoop
+// find coordinates of cone
+
+var $scoop = $('#scoop');
+var scoop = document.querySelector('#scoop');
+var $cone = $('#cone');
+var cone = document.querySelector('#cone');
+var $scoopRef = $('.scoopRef');
+
+
+function detectColl() {
+if (cone.x < scoop.x + scoop.width &&
+   cone.x + cone.width > scoop.x &&
+   cone.y < scoop.y + scoop.height &&
+   cone.height + cone.y > scoop.y) {
+  console.log('detected');
+    // collision detected!
+}
+
+// filling in the values
+// scoop values < cone values
+
+if (8 > -40 &&
+    85 < 180 &&
+    540 < 1330 &&
+    85 < 155) {
+    // collision detected!
+
+  console.log('detected for real');
+  var imageOffset1 = 0;
+    return function () {
+      return imageOffset1 -= 20;
+      var scoopRef = document.querySelectorAll('.scoopRef');
+      scoopRef.style.left = imageOffset + "px";
+    }
+
+} else {
+  // no collision
+  console.log('nothing');
+  // document.getElementById("cone").style.border = "red";
+  }
+
+};
+
+detectColl();
+
+// create new class for scoops with collision detection
+// attempt to make the scoop move with the cone
+// this does not work
+// keep getting error in console.log
+
+
+function leftArrowScoop() {
+  imageOffset -= 20;
+  // margin-left -=2;
+  let scoopRef = document.querySelectorAll('.scoopRef');
+      scoopRef.style.left = imageOffset + "px";
+
+}
+
+leftArrowScoop();
+
+// CREATING AN ARRAYS FOR ICE CREAM SCOOPS AND CONE
+// second option, since class name did not keep scoops on cone
+// array flavors will help keep scoop on cone
+// array score will track score
+
+var flavors = [1, 2, 3, 4, 5];
+var cone = [];
+var score = [];
+
+flavors.push();
+
+
---
+++ pseudoCode.js
@@ -0,0 +1,95 @@
+
+// Psuedo Code
+
+// ********************** LANDING VIEW  **************************
+
+// div for landing view
+// create 'start game' button
+// close div for landing view
+
+// add event listener 'click'
+// 'start game' button sends player to the form page
+
+// ********************** FORM VIEW  ****************************
+// div for form view
+// form to enter player's name
+// use input value
+// create 'submit' button
+// close div for form view
+
+// add event listener 'click'
+// 'submit' button sends player to the game view
+
+// ********************** GAME VIEW  ****************************
+
+// create a constructor function on JS file
+
+// CONE
+// create a div for the CONE
+
+// make the cone an array on JS
+// create a key press function event
+// the key press event will do the following:
+// left arrow moves the cone to the left
+// right arrow moves the cone to the right
+
+// ICE CREAM SCOOPS
+// create many divs - one div for each ice cream scoop each
+// deciding whether to make the ice cream scoops circles... or use a scoop image
+// this will control the timing of the ice cream scoops falling on the screen
+
+// create a setInterval( function NAME, _________ set the ms time)
+// use a constructor function NAME to increment the y coordinates (moving down)
+// and cause the ice cream scoops to fall down the screen
+
+// ************* CREATE AN ARRAY ****************
+
+// The images of scoops will be objects
+// store the objects into an array
+// set values or numbers into the array to keep score of the scoops
+
+// ************* MAKE THE SCOOPS STAY ON THE CONE ****************
+
+// detection collision
+
+
+
+// sample code for keypress event
+// <script>
+
+// 'use strict';
+
+// document.addEventListener('keypress', (event) => {
+//   const keyName = event.key;
+
+//   alert('keypress event\n\n' + 'key: ' + keyName);
+// });
+
+// </script>
+
+// ********************** RESPONSIVENESS  ****************************
+
+// *******************************************************************
+// learn how to make the game responsive
+// maybe use touch screen (?) or tilt phone to make cone move
+// use mediaquery
+// I will probably make the screen small so that it will be mobile friendly
+// *******************************************************************
+
+// PHYSICS - COlLISION DETECTION
+// create a setInterval()
+// set the ms time
+// this will control the timing of the ice cream scoops falling on the screen
+// use a constructor function to increment the y coordinates
+// and cause the ice cream scoops to fall down the screen
+
+// ******************** WIN OR LOSE ALERT VIEW  **********************
+// create alert('You win!') if statement - if the player wins
+// create alert('You lose!') if statement - if the player loses
+// create 'replay' button
+
+// add event listener 'click'
+// 'replay' button sends player to the game view
+
+
+
